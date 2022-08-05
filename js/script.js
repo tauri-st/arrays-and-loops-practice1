c8d28d2c14f3d081a4f2a898f1169c8152bf3d6d
tauri_s <tauristclaire@gmail.com>
2022-08-04 17:41:13 -0700

    Logout every other item in index

--- js/script.js
+++ js/script.js
@@ -9,3 +9,10 @@ var stuff = [
   "coffee",
   "books"
 ];
+
+//Instructions: Log out to the console each element of the stuff array with even-numbered indexes.
+stuff.forEach(function (item, index) {
+  if (index % 2 === 0) {
+    console.log(item);
+  }
+});
